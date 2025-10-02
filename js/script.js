f3f7d347c3eef55294ff0affb033f00bc63f8535
Darya <145912572+Dashkere404@users.noreply.github.com>
2025-10-02 18:10:08 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('theme-toggle').addEventListener('click', function () {
+  document.body.classList.toggle('dark-theme');
+});
+
+// Дополнительно: можно добавить сохранение темы в localStorage
